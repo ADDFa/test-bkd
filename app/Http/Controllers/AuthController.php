7807e0ead7fe6d52551f971129fd7124dbda7d82
ADDFa <fratamaadha18@gmail.com>
2025-02-19 12:12:39 +0700

    set url

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
     public function getPns()
     {
         $nip = "198509272011012009";
-        $ch = curl_init("http://epresensi.bengkuluprov.go.id:94/v1/login/{$nip}");
+        $ch = curl_init("http://epresensi.bengkuluprov.go.id:94/v1/test/{$nip}");
 
         $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9lcHJlc2Vuc2kuYmVuZ2t1bHVwcm92LmdvLmlkOjk0XC92MVwvbG9naW4iLCJpYXQiOjE3Mzk5NDEyMjEsImV4cCI6MTczOTk0NDgyMSwibmJmIjoxNzM5OTQxMjIxLCJqdGkiOiJMM0dHckdSVlhkbnl6ckRFIiwic3ViIjoiNzcwYjY4M2UtMjI1ay00NG5tLWE1MDAtZTU2YzI2Njc4ODM0IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.3RVd_TJB7oW6B63UcagdTb8tUJraK_C2v8nUxWutz5s";
 
